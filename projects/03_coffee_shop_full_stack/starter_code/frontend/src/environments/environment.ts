b477fa4e33189eb37ee00ec3737aa9fca0a67241
omar1128-gif <o_fathy28@yahoo.com>
2020-09-24 18:48:48 +0200

    remove all todos and finish project

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
     url: 'dev-3l7-fwbc.eu', // the auth0 domain prefix
     audience: 'drinks', // the audience set for the auth0 app
     clientId: 'arOL1NZGQ6dC3CXRaTYXZNQ76KO7cuIh', // the client id generated for the auth0 app
-    callbackURL: 'http://127.0.0.1:8100/tabs/user-page', // the base url of the running ionic application.
+    callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application.
   }
 };
